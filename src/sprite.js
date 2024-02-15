55976cb73fad5cd55d6941c83cd2730e37f75515
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-02-15 23:58:19 +0530

    Update sprite.js

--- src/sprite.js
+++ src/sprite.js
@@ -1,20 +1,64 @@
+import { Vector2 } from "./vector2";
+
 export class Sprite {
     constructor({
         resource,  // image we want tp draw
-        frameSize, // size of the image
+        frameSize, // size of the image in sprite sheet
         hFrames,   // arrangement of sprite horizontally
         vFrames,   // arrangement of sprite vertically
         frame,     // tileId of frame we want to show
-        scale,
+        scale,     
         position,
     }) {
         this.resource = resource;
-        this.frameSize = frameSize;
+        this.frameSize = frameSize ?? new Vector2(16, 16);
         this.hFrames = hFrames ?? 1;
         this.vFrames = vFrames ?? 1;
         this.frame = frame ?? 0;
         this.frameMap = new Map();
         this.scale = scale ?? 1;
-        this.position = position;
+        this.position = position ?? new Vector2(0, 0);
+        this.buildframeMap();  // call to build method in constructor 
+    }
+
+    buildframeMap() {
+        let frameCount = 0;
+        for (let v = 0; v < this.vFrames; v += 1) {
+            for (let h = 0; h < this.hFrames; h += 1) {
+
+                this.frameMap.set(
+                    frameCount,
+                    new Vector2(this.frameSize.x * h, this.frameSize.y * v),
+                );
+                frameCount ++;
+            }
+        }
+    }
+
+    drawImage(ctx, x, y) {
+        if(!this.resource.isLoaded) {
+            return;
+        }
+        // default for error handling
+        let frameCoordX = 0;
+        let frameCoordY = 0;
+
+        const frame = this.frameMap.get(this.frame);
+        if (frame) {
+            frameCoordX = frame.x;
+            frameCoordY = frame.y;
+        }
+
+        ctx.drawImage(
+            this.resource.image,
+            frameCoordX,
+            frameCoordY,
+            this.frameSize.x,
+            this.frameSize.y,
+            x,
+            y,
+            this.frameSize.x * this.scale,
+            this.frameSize.y * this.scale,
+        )
     }
 }
