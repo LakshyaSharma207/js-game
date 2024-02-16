00e1ceb1ded84d82ea0c6625743bfaad367fce4c
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-02-17 00:08:52 +0530

    Create level1.js

---
+++ src/levels/level1.js
@@ -0,0 +1,13 @@
+export const walls = new Set();
+
+walls.add(`64,48`); // tree
+
+walls.add(`64,64`); // squares
+walls.add(`64,80`);
+walls.add(`80,64`);
+walls.add(`80,80`);
+
+walls.add(`112,80`); // water
+walls.add(`128,80`);
+walls.add(`144,80`);
+walls.add(`160,80`);
