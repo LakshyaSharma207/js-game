a88cb755ee76d4d148f4b5b9319c9b97adfb4539
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-02-17 00:09:00 +0530

    Update vector2.js

--- src/vector2.js
+++ src/vector2.js
@@ -1,9 +1,10 @@
 export class Vector2{
-    constructor(
-        x = 0,
-        y = 0,
-    ){
+    constructor(x = 0, y = 0){
         this.x = x;
         this.y = y;
+    }
+
+    duplicate() {
+        return new Vector2(this.x, this.y);
     }
 }
