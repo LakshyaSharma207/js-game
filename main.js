a940c3d07dd153234c22934e02e9df3b8e0057c9
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-02-17 00:08:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,54 @@
 import { GameLoop } from './src/gameLoop';
+import { gridCells } from './src/helpers/grid';
 import { KeyInput } from './src/keyInput';
 import { resources } from './src/resource';
 import { Sprite } from './src/sprite';
 import { Vector2 } from './src/vector2';
 import './style.css'
+import { GameObject } from './src/gameObject.js';
+import { Hero } from './src/objects/hero/hero.js';
 
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
+
+const mainScene = new GameObject({
+  position: new Vector2(0, 0)
+});
+
+const skySprite = new Sprite({
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180)
+})
 
 const groundSprite = new Sprite({
   resource: resources.images.ground,
   frameSize: new Vector2(320, 180),
 });
+mainScene.addChild(groundSprite);
 
-const hero = new Sprite({
-  resource: resources.images.hero,
-  frameSize: new Vector2(32, 32),
-  hFrames: 3,
-  vFrames: 8,
-  frame: 1,
-});
+// hero sprite
+const hero = new Hero(gridCells(6), gridCells(5));
+mainScene.addChild(hero);
 
-const shadowSprite = new Sprite({
-  resource: resources.images.shadow,
-  frameSize: new Vector2(32, 32),
-});
-
-const heroPos = new Vector2(16 * 6, 16 * 5);
+// add input class to move character
+mainScene.input = new KeyInput();
 
 const draw =  () => {
-  groundSprite.drawImage(ctx, 0, 0);
+  // Clear anything stale
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  hero.drawImage(ctx, heroPos.x - 8, heroPos.y - 21);
-  shadowSprite.drawImage(ctx, heroPos.x - 8, heroPos.y - 21);
+  // Draw the sky
+  skySprite.drawImage(ctx, 0, 0)
+
+  // draw all objects in main scene
+  mainScene.draw(ctx, 0, 0);
 }
 
-const keyInput = new KeyInput();
-const update = () => {
-  // console.log(keyInput.direction);
-  if (keyInput.direction === "UP") {
-    heroPos.y -= 1;
-    hero.frame = 6;
-  } 
-  else if (keyInput.direction === "DOWN") {
-      heroPos.y += 1;
-      hero.frame = 0;
-  } 
-  else if (keyInput.direction === "LEFT") {
-      heroPos.x -= 1;
-      hero.frame = 9;
-  } 
-  else if (keyInput.direction === "RIGHT") {
-      heroPos.x += 1;
-      hero.frame = 3;
-  }
+// updating game state
+const update = (delta) => {
+  mainScene.stepEntry(delta, mainScene);
 }
+
 
 const gameLoop = new GameLoop(update, draw);
 gameLoop.start();
