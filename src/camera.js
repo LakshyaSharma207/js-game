3cdbe95803f56132b64d7c9b909bc1a9a4e3c449
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-02-18 16:40:12 +0530

    Create camera.js

---
+++ src/camera.js
@@ -0,0 +1,23 @@
+import { events } from "./events";
+import { GameObject } from "./gameObject";
+import { Vector2 } from "./vector2";
+
+export class Camera extends GameObject {
+    constructor() {
+        super({});
+
+        events.on("hero_position", this, (heroPosition) => {
+
+            // create a new position based on hero position
+            const halfWidth = -8 + 320 / 2;
+            const halfHeight = -8 + 180 / 2;
+
+            // console.log("Moved, ", heroPosition);
+
+            this.position = new Vector2(
+                -heroPosition.x + halfWidth,
+                -heroPosition.y + halfHeight
+            )
+        })
+    }
+}
