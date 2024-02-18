3ca1a1b290aa94b033c69d614085690072d9624c
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-02-18 16:40:20 +0530

    Update gameObject.js

--- src/gameObject.js
+++ src/gameObject.js
@@ -1,9 +1,12 @@
+import { events } from "./events";
 import { Vector2 } from "./vector2";
 
 export class GameObject {
     constructor({ position }) {
         this.position = position ?? new Vector2(0, 0);
         this.children = [];
+        this.parent = null;
+        this.isReady = false;
     }
 
     // first entry of loop
@@ -11,12 +14,18 @@ export class GameObject {
         // call updates on all children first
         this.children.forEach((child) => child.stepEntry(delta, root));
 
+        // call ready state
+        if (!this.isReady) {
+            this.isReady = true;
+            this.ready();
+        }
+
         this.step(delta, root);
     }
-    
-    step(_delta) {
 
-    }
+    ready() {}
+    
+    step(_delta) {}
 
     draw(ctx, x, y) {
         const drawPosX = x + this.position.x;
@@ -29,12 +38,22 @@ export class GameObject {
         this.children.forEach((child) => child.draw(ctx, drawPosX, drawPosY));
     }
     
-    drawImage(ctx, drawPosX, drawPosY) {
-    //...
-    }
+    drawImage(ctx, drawPosX, drawPosY) {}
 
     addChild(gameObject) {
+        gameObject.parent = this;
         this.children.push(gameObject);
+    }
+
+    destroy(gameObject) {
+        this.children.forEach((child) => child.destroy())
+
+        this.parent.remove(this);
+    }
+
+    remove(gameObject) {
+        events.unsubscribe(gameObject);
+        this.children = this.children.filter((child) => gameObject !== child);
     }
     
 }
